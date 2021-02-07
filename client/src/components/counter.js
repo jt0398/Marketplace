18d67c9b56e949f25459a02d059060f7ef2869df
Judy <judys717@hotmail.com>
2021-02-07 07:52:17 -0500

    Add Delete button

--- client/src/components/counter.js
+++ client/src/components/counter.js
@@ -16,7 +16,6 @@ class Counter extends Component {
 
   handleIncrement(product) {
     this.setState({ value: this.state.value + 1 });
-    console.log("product id " + product.id);
   }
 
   render() {
@@ -28,6 +27,12 @@ class Counter extends Component {
           className="btn btn-secondary btn-sm"
         >
           Increment
+        </button>
+        <button
+          onClick={() => this.props.onDelete(this.props.id)}
+          className="btn btn-danger btn-sm ml-2"
+        >
+          Delete
         </button>
       </div>
     );
--- client/src/components/counters.js
+++ client/src/components/counters.js
@@ -10,11 +10,27 @@ class Counters extends Component {
       { id: 4, value: 0 },
     ],
   };
+
+  constructor() {
+    super();
+    this.handleDelete = this.handleDelete.bind(this);
+  }
+
+  handleDelete(id) {
+    const counters = this.state.counters.filter((counter) => counter.id !== id);
+    this.setState({ counters });
+  }
+
   render() {
     return (
       <div>
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} value={counter.value} />
+          <Counter
+            key={counter.id}
+            value={counter.value}
+            id={counter.id}
+            onDelete={this.handleDelete}
+          />
         ))}
       </div>
     );
