5d563d632a3fc54c77ec797cf42b14962353bb26
Judy <judys717@hotmail.com>
2021-02-21 07:55:40 -0500

    Change like component to stateless function

--- client/src/components/like.js
+++ client/src/components/like.js
@@ -1,19 +1,17 @@
-import React, { Component } from "react";
+import React from "react";
 
-class Like extends Component {
-  render() {
-    let classes = "fa fa-heart";
-    if (!this.props.liked) classes += "-o";
+const Like = (props) => {
+  let classes = "fa fa-heart";
+  if (!props.liked) classes += "-o";
 
-    return (
-      <i
-        className={classes}
-        aria-hidden="true"
-        onClick={this.props.onClick}
-        style={{ cursor: "pointer" }}
-      ></i>
-    );
-  }
-}
+  return (
+    <i
+      className={classes}
+      aria-hidden="true"
+      onClick={props.onClick}
+      style={{ cursor: "pointer" }}
+    ></i>
+  );
+};
 
 export default Like;
